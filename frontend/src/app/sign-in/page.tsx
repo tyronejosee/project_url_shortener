79c194f39e5a8f74fb8d7bec209893991d09aac2
tyronejose <alt.tyronejose@gmail.com>
2025-02-19 21:24:34 -0300

    feat: add structure and styles for sign-up and sign-in pages

--- frontend/src/app/sign-in/page.tsx
+++ frontend/src/app/sign-in/page.tsx
@@ -1,7 +1,96 @@
-export default function SignInPage() {
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Button } from "@heroui/button";
+import { Input } from "@heroui/input";
+import { Eye, EyeClosed } from "lucide-react";
+
+export default function SignIn() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
   return (
-    <section className="max-w-screen-2xl mx-auto text-center py-6 px-6">
-      <h1>Sign In</h1>
-    </section>
+    <main className="max-w-screen-lg mx-auto p-6 flex justify-center">
+      <section className="w-full max-w-md p-8 rounded-2xl border border-neutral-300">
+        <h2 className="text-4xl font-bold text-center text-gray-800 mb-2">
+          Welcome Back!
+        </h2>
+        <p className="text-center text-gray-500 mb-12">
+          Sign in to access all the features of the service.
+        </p>
+
+        <form
+          // onSubmit={handleSubmit}
+          className="space-y-6"
+        >
+          <div className="space-y-9">
+            <Input
+              isRequired
+              // isDisabled={isLoading}
+              label="Email"
+              value={email}
+              labelPlacement="outside"
+              name="username"
+              placeholder="you@example.com"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              endContent={
+                <button
+                  aria-label="toggle password visibility"
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={toggleVisibility}
+                >
+                  {isVisible ? (
+                    <Eye className="text-default-400 pointer-events-none" />
+                  ) : (
+                    <EyeClosed className="text-default-400 pointer-events-none" />
+                  )}
+                </button>
+              }
+              isRequired
+              // isDisabled={isLoading}
+              type={isVisible ? "text" : "password"}
+              label="Password"
+              value={password}
+              labelPlacement="outside"
+              name="password"
+              placeholder="********"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="text-sm">
+              <Link
+                href="/forgot-password"
+                className="font-medium text-primary hover:underline"
+              >
+                Forgot your password?
+              </Link>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <Button type="submit" color="primary" className="w-full">
+              Sign In
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6 text-center">
+          <p className="text-sm text-gray-600">
+            Don&apos;t have an account?{" "}
+            <Link href="/sign-up" className="text-primary hover:underline">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </section>
+    </main>
   );
 }
--- frontend/src/app/sign-up/page.tsx
+++ frontend/src/app/sign-up/page.tsx
@@ -1,7 +1,119 @@
-export default function SignUpPage() {
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Button } from "@heroui/button";
+import { Input } from "@heroui/input";
+import { Eye, EyeClosed } from "lucide-react";
+
+export default function SignUp() {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
   return (
-    <section className="max-w-screen-2xl mx-auto text-center py-6 px-6">
-      <h1>Sign Up</h1>
-    </section>
+    <>
+      <main className="max-w-screen-lg mx-auto p-6 flex justify-center">
+        <section className="w-full max-w-md bg-white p-8 rounded-2xl border border-neutral-300">
+          <h2 className="text-4xl font-bold text-center text-gray-800 mb-2">
+            Create an Account
+          </h2>
+          <p className="text-center text-gray-500 mb-12">
+            Sign up to access all the features of the service.
+          </p>
+          <form
+            // onSubmit={handleSubmit}
+            className="space-y-6"
+          >
+            <div className="space-y-9">
+              <Input
+                isRequired
+                label="Name"
+                value={name}
+                labelPlacement="outside"
+                name="name"
+                placeholder="Joe Doe"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <Input
+                isRequired
+                label="Email"
+                value={email}
+                labelPlacement="outside"
+                name="email"
+                placeholder="you@example.com"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                endContent={
+                  <button
+                    aria-label="toggle password visibility"
+                    className="focus:outline-none"
+                    type="button"
+                    onClick={toggleVisibility}
+                  >
+                    {isVisible ? (
+                      <Eye className="text-default-400 pointer-events-none" />
+                    ) : (
+                      <EyeClosed className="text-default-400 pointer-events-none" />
+                    )}
+                  </button>
+                }
+                isRequired
+                type={isVisible ? "text" : "password"}
+                label="Password"
+                value={password}
+                labelPlacement="outside"
+                name="password"
+                placeholder="********"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Input
+                endContent={
+                  <button
+                    aria-label="toggle password visibility"
+                    className="focus:outline-none"
+                    type="button"
+                    onClick={toggleVisibility}
+                  >
+                    {isVisible ? (
+                      <Eye className="text-default-400 pointer-events-none" />
+                    ) : (
+                      <EyeClosed className="text-default-400 pointer-events-none" />
+                    )}
+                  </button>
+                }
+                isRequired
+                type={isVisible ? "text" : "password"}
+                label="Confirm Password"
+                value={confirmPassword}
+                labelPlacement="outside"
+                name="confirm-password"
+                placeholder="********"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="flex items-center justify-between">
+              <Button type="submit" color="primary" className="w-full">
+                Sign Up
+              </Button>
+            </div>
+          </form>
+
+          <div className="mt-6 text-center">
+            <p className="text-sm text-gray-600">
+              Already have an account?{" "}
+              <Link href="/sign-in" className="text-primary hover:underline">
+                Sign In
+              </Link>
+            </p>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
