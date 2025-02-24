77d7d77547880430a26d7453f76f08924cc5daeb
tyronejose <alt.tyronejose@gmail.com>
2025-02-24 20:43:11 -0300

    feat: add cutter and groups pages

--- frontend/src/app/dashboard/cutter/page.tsx
+++ frontend/src/app/dashboard/cutter/page.tsx
@@ -1,5 +1,64 @@
 "use client";
 
+import { Button } from "@heroui/button";
+import { Input } from "@heroui/input";
+import { Select, SelectItem } from "@heroui/select";
+import { domains, groups, privacyItems } from "@/config/constants";
+
 export default function CutterPage() {
-  return <h1>Cutter</h1>;
+  return (
+    <main className="max-w-screen-md mx-auto p-6">
+      <h1 className="text-4xl font-bold text-center pb-10">URL Cutter</h1>
+      <form className="space-y-6">
+        <Input
+          isRequired
+          label="URL"
+          labelPlacement="outside"
+          name="username"
+          placeholder="https://www.example.com"
+        />
+        <div className="flex space-x-6">
+          <Input
+            type="password"
+            label="Password"
+            labelPlacement="outside"
+            name="password"
+            placeholder="********"
+          />
+          <Select
+            items={privacyItems}
+            label="Privacy"
+            labelPlacement="outside"
+            placeholder="Select a Domain"
+            defaultSelectedKeys={["public"]}
+          >
+            {(item) => <SelectItem key={item.key}>{item.label}</SelectItem>}
+          </Select>
+        </div>
+        <div className="flex space-x-6">
+          <Select
+            isDisabled
+            items={domains}
+            label="Domain"
+            labelPlacement="outside"
+            placeholder="Select a Domain"
+          >
+            {(domain) => (
+              <SelectItem key={domain.id}>{domain.domain}</SelectItem>
+            )}
+          </Select>
+          <Select
+            isDisabled
+            items={groups}
+            label="Group"
+            labelPlacement="outside"
+            placeholder="Select a Group"
+          >
+            {(group) => <SelectItem key={group.id}>{group.name}</SelectItem>}
+          </Select>
+        </div>
+        <Button color="primary">Cut</Button>
+      </form>
+    </main>
+  );
 }
--- frontend/src/app/dashboard/groups/page.tsx
+++ frontend/src/app/dashboard/groups/page.tsx
@@ -1,5 +1,53 @@
 "use client";
 
+import React from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@heroui/table";
+import { groups } from "@/config/constants";
+import { Chip } from "@heroui/chip";
+
 export default function GroupsPage() {
-  return <h1>Groups</h1>;
+  return (
+    <div className="flex flex-col gap-3">
+      <Table
+        aria-label="Groups Table"
+        color="primary"
+        selectionMode="single"
+        radius="lg"
+        shadow="none"
+        className="border border-neutral-300 rounded-xl"
+      >
+        <TableHeader>
+          <TableColumn>Name</TableColumn>
+          <TableColumn>Description</TableColumn>
+          <TableColumn>Links</TableColumn>
+          <TableColumn>Is Active</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {groups.map((group) => (
+            <TableRow key={group.id}>
+              <TableCell>{group.name}</TableCell>
+              <TableCell>{group.description}</TableCell>
+              <TableCell>{group.links}</TableCell>
+              <TableCell>
+                <Chip
+                  size="sm"
+                  color={group.is_active ? "success" : "danger"}
+                  variant="flat"
+                >
+                  {group.is_active ? "Active" : "Inactive"}
+                </Chip>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
