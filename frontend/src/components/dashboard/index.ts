94f9162226f021c47e64bb576c75d228a93cab5c
tyronejose <alt.tyronejose@gmail.com>
2025-03-18 20:06:56 -0300

    feat: add dashboard skeleton component and social media icons for facebook and google

--- frontend/src/components/dashboard/index.ts
+++ frontend/src/components/dashboard/index.ts
@@ -1,3 +1,4 @@
 export { default as GroupTable } from "./GroupTable";
 export { default as BarChartGraphic } from "./BarChartGraphic";
 export { default as PieChartGraphic } from "./PieChartGraphic";
+export { default as DashboardSkeleton } from "./DashboardSkeleton";
