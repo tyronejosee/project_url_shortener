cff6ec7293dbdf93413a4def4470baf990c6e4da
tyronejose <alt.tyronejose@gmail.com>
2025-04-02 22:31:52 -0300

    feat: enhance authentication middleware and extend authjs types for user session management

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,13 +1,46 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import NextAuth from "next-auth";
+import authConfig from "@/auth-config";
 
-export async function middleware(req: NextRequest) {
-  const token = req.cookies.get("refresh");
-  if (!token) {
-    return NextResponse.redirect(new URL("/auth/login", req.url));
+const { auth } = NextAuth(authConfig);
+
+const publicRoutes = ["/", "/prices"];
+const authRoutes = ["/auth/login", "/auth/register"];
+const apiAuthPrefix = "/api/auth";
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  // console.log({ isLoggedIn, path: nextUrl.pathname });
+
+  // Permit all authentication API routes
+  if (nextUrl.pathname.startsWith(apiAuthPrefix)) {
+    return NextResponse.next();
   }
+
+  // Permit access to public routes regardless of authentication state
+  if (publicRoutes.includes(nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+
+  // Redirect to /dashboard if the user is logged in and tries to access an authentication route
+  if (isLoggedIn && authRoutes.includes(nextUrl.pathname)) {
+    return NextResponse.redirect(new URL("/dashboard", nextUrl));
+  }
+
+  // Redirect to /login if the user is not logged in and tries to access a protected route
+  if (
+    !isLoggedIn &&
+    !authRoutes.includes(nextUrl.pathname) &&
+    !publicRoutes.includes(nextUrl.pathname)
+  ) {
+    return NextResponse.redirect(new URL("/auth/login", nextUrl));
+  }
+
   return NextResponse.next();
-}
+});
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
