28455b8e30278275a19efde639da87add7a5b134
tyronejose <alt.tyronejose@gmail.com>
2025-06-04 18:08:39 -0400

    feat: add DomainDetailView for retrieving, updating, and deleting specific domains

--- backend/apps/domains/urls.py
+++ backend/apps/domains/urls.py
@@ -3,12 +3,16 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 
-from .views import DomainListView
+from .views import DomainListView, DomainDetailView
 
 
 urlpatterns: list[URLPattern] = [
     path(
         "api/domains",
         DomainListView.as_view(),
+    ),
+    path(
+        "api/domains/<str:domain_id>",
+        DomainDetailView.as_view(),
     ),
 ]
--- backend/apps/domains/views.py
+++ backend/apps/domains/views.py
@@ -1,5 +1,6 @@
 """Views for Domains App."""
 
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -37,3 +38,33 @@ class DomainListView(APIView):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DomainDetailView(APIView):
+    """
+    View to update, and delete a specific domain.
+
+    Endpoints:
+    - PATCH /api/domains/{id}
+    - DELETE /api/domains/{id}
+    """
+
+    def get_object(self, pk: str) -> Domain:
+        return get_object_or_404(Domain, pk=pk)
+
+    def patch(self, request: Request, domain_id: str) -> Response:
+        domain = self.get_object(domain_id)
+        serializer = DomainWriteSerializer(
+            domain,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, domain_id: str) -> Response:
+        domain = self.get_object(domain_id)
+        domain.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
